f6b946785c8377eee9022b629627d05be564e35b
Mathew J Kelly <Matthew.Kelly@washpost.com>
2017-10-26 14:35:09 -0400

    Improve OOP implementation.

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -9,10 +9,10 @@ resource_tree = dict()
 direct_descendants = dict()
 
 interesting_fields = {'Instance':['BlockDeviceMappings','ImageId','SecurityGroups'],
-                    'Volume':['','',],
-                    'Snapshot':['','',],
-                    'Image':['','',],
-                    'SecurityGroup':['','',],
+                    'Volume':['SnapshotId',],
+                    'Snapshot':[],
+                    'Image':[],
+                    'SecurityGroup':[],
                     }
 
 fuzzy_fields = {'Instance':{'_id':'InstanceId','_time':'LaunchTime'},
@@ -39,7 +39,7 @@ def get_arn_type(arn):
         return 'SecurityGroup'
 
 
-def fetch_asset_obj(arn):
+def get_payload_obj(arn):
     arn_type = get_arn_type(arn)
     if arn_type == 'Instance':
         return Instance(arn)
@@ -62,18 +62,32 @@ def extract_arns(field, value_section):
         return volume_ids
     elif field == 'ImageId':
         return [value_section] 
+    elif field == 'SnapshotId':
+        return [value_section]
     elif field == 'SecurityGroups':
         return [group['GroupId'] for group in value_section]
     else:
         return list()
 
 
-def flatten_tags(tags=None):
-    if tags is None or len(tags) == 0:
-        return ''
-    tag_list = [list(x) for x in [y.values() for y in tags]]
-    formatted_tags = [str(val[0])+'='+str(val[1]) for val in tag_list]
-    return '\n'.join(formatted_tags)
+def build_resource_tree(node):
+    """
+    args:
+        root_arn : amazon resource name as the root of the resource tree
+    returns:
+        resource_tree : nested dicitonaries from root to leaf nodes plus metadata
+    """
+    children = node.find_children()
+    if len(children) == 0:
+        return node
+    for child in children:
+        child_node = build_resource_tree(get_payload_obj(child))
+        node.children[child] = child_node
+    return node
+
+
+class ZeroReservationError(TypeError):
+    pass
 
 
 class Asset:
@@ -83,23 +97,40 @@ class Asset:
     def __init__(self, arn):
 
         # TODO move this
-        if arn in direct_descendants.keys():
+        if arn in resources_found:
             print("Already found ", arn)
             return None
         else:
-            direct_descendants[arn] = list()
+            resources_found.append(arn)
 
         self.arn = arn
-        self.children = None
-        self.tag_str = None
-        self.payload = None
+        self.children = dict() 
+
         self.ref_time = None
-        self.asset_id = None
 
     def get_asset_id(self):
         asset_type = self._type
         id_field = fuzzy_fields[asset_type]['_id']
         return self.payload[id_field]
+
+    def find_children(self):
+        child_arns = list()
+        search_keys = interesting_fields[self._type]
+        keys_found = [k for k in search_keys if k in self.payload.keys()]
+        for k in keys_found:
+            vs = extract_arns(k, self.payload[k])
+            child_arns += vs
+        return child_arns 
+
+    def add_tag_str(self):
+        if 'Tags' not in self.payload.keys(): 
+            return ''
+        tags = self.payload['Tags']
+        if tags is None or len(tags) == 0:
+            return ''
+        tag_list = [list(x) for x in [y.values() for y in tags]]
+        formatted_tags = [str(val[0])+'='+str(val[1]) for val in tag_list]
+        self.tag_str = '\n'.join(formatted_tags)
 
 
 class Instance(Asset):
@@ -108,17 +139,20 @@ class Instance(Asset):
     """
     def __init__(self, arn):
         super().__init__(arn)
+        self._type = "Instance"
+        self.get_payload()
+        self.add_tag_str() 
 
-    def fetch_asset(self):
+    def get_payload(self):
         try:
             payload = ec2.describe_instances(InstanceIds=[self.arn])
         except:
             print('Instance does not exist.')
             return None
         
-        payload.keys()
         if 'Reservations' not in payload.keys() or len(payload['Reservations'])==0:
             print('Zero reservations.')
+            # raise ZeroReservationError('No reservations found in {} payload.'.format(arn))
             return None
 
         if len(payload['Reservations'])>1:
@@ -134,7 +168,13 @@ class Volume(Asset):
     """
 
     """
-    def fetch_asset(self):
+    def __init__(self, arn):
+        super().__init__(arn)
+        self._type = "Volume"
+        self.get_payload()
+        self.add_tag_str() 
+
+    def get_payload(self):
         try:
             payload = ec2.describe_volumes(VolumeIds=[self.arn])
         except:
@@ -155,7 +195,13 @@ class Snapshot(Asset):
     """
 
     """
-    def fetch_asset(self):
+    def __init__(self, arn):
+        super().__init__(arn)
+        self._type = "Snapshot"
+        self.get_payload()
+        self.add_tag_str() 
+
+    def get_payload(self):
         try:
             payload = ec2.describe_snapshots(SnapshotIds=[self.arn])
         except:
@@ -176,7 +222,13 @@ class Image(Asset):
     """
 
     """
-    def fetch_asset(self):
+    def __init__(self, arn):
+        super().__init__(arn)
+        self._type = "Image"
+        self.get_payload()
+        self.add_tag_str() 
+
+    def get_payload(self):
         try:
             payload = ec2.describe_images(ImageIds=[self.arn])
         except:
@@ -197,7 +249,13 @@ class SecurityGroup(Asset):
     """
 
     """
-    def fetch_asset(self):
+    def __init__(self, arn):
+        super().__init__(arn)
+        self._type = "SecurityGroup"
+        self.get_payload()
+        self.add_tag_str() 
+
+    def get_payload(self):
         try:
             payload = ec2.describe_security_groups(GroupIds=[self.arn])
         except:
@@ -218,66 +276,11 @@ if __name__ == '__main__':
     """
 
     """
-    arns = list()
+    root_arn = 'i-836b9d9b'
 
-    # arns.append('vol-596aeefe')# volume eg
-    # arns.append('snap-25dd2ac1')# volume eg
-    # arns.append('i-836b9d9b')# instance eg
-    # arns.append('ami-6869aa05')
-    # arns.append('i-836b9d9b')# instance eg
+    this_asset = get_payload_obj(root_arn)
+    resource_tree = build_resource_tree(this_asset)
 
-    arns.append('i-0b2efb42fb389c3fb')
-    # arns.append('vol-0e566a70d756391eb')
-    # arns.append('vol-0151f28dcf5e7ba9d')
-    # arns.append('ami-7c8a776a')
-    # arns.append('sg-525be129')
-
-    assets = {}
-
-    while True:
-        if len(arns)==0:
-            break;
-        else:
-            arn = arns.pop()
-            this_asset = fetch_asset_obj(arn)
-            this_asset.fetch_asset()
-            pp(this_asset.payload)
-            search_keys = interesting_fields[get_arn_type(arn)]
-            keys_found = [k for k in search_keys if k in this_asset.payload.keys()]
-            for k in keys_found:
-                vs = extract_arns(k, this_asset.payload[k])
-                pp(vs)
-                 
-                [arns.append(v) for v in vs] 
-                
-
-    sys.exit()
-    for arn in arns:
-        print('\n\nFetching {}:'.format(arn))
-        this_asset = fetch_asset_obj(arn)
-        this_asset.fetch_asset()
-
-        this_asset._type = get_arn_type(arn)
-        # this_asset._id = this_asset.get_asset_id()
-        assets[arn] = this_asset 
-  
-
-
-
-     
-    pp(assets)
-    print(direct_descendants) 
-    sys.exit()
-
-    for a in assets:
-        if 'Tags' in a.keys():
-            print()
-            print(a['asset_is'])
-            print(flatten_tags(a['Tags']))
-
-
-    r = Instance(arn)
-    r.print_asset_arn()
-    print(r.arn_type())
-    r.fetch_asset()
+    pp(resource_tree.children['vol-596aeefe'].tag_str)
+    # pp(resource_tree.tag_str)
 
